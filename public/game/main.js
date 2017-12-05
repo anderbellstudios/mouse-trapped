d51301fcbd3e91ecd2ab2fbd853e52ffd35f5af1
Joe Anderson <joejbajoe@gmail.com>
2017-12-05 08:55:05 +0000

    Refactor duplication of menu starting code

--- public/game/main.js
+++ public/game/main.js
@@ -5,25 +5,29 @@ var user_settings = {
   sounds_enabled: true
 }
 
+function launch_menu(name, buttons) {
+  game.state.start('menu', true, false, name, buttons);
+}
+
 var main_menu = [
   { name: 'play',    onclick: function () { game.state.start('playing', true, false, '01'); } },
-  { name: 'options', onclick: function () { game.state.start('menu', true, false, 'options', options_menu); } }
+  { name: 'options', onclick: function () { launch_menu('options', options_menu); } }
 ];
 
 var options_menu = [
-  { name: 'music',  onclick: function () { game.state.start('menu', true, false, 'music', music_menu); } },
-  { name: 'sounds', onclick: function () { game.state.start('menu', true, false, 'sounds', sounds_menu); } },
-  { name: 'back',   onclick: function () { game.state.start('menu', true, false, 'main_menu', main_menu); } }
+  { name: 'music',  onclick: function () { launch_menu('music', music_menu); } },
+  { name: 'sounds', onclick: function () { launch_menu('sounds', sounds_menu); } },
+  { name: 'back',   onclick: function () { launch_menu('main_menu', main_menu); } }
 ];
 
 var music_menu = [
-  { name: 'on',  onclick: function () { user_settings.music_enabled = true;  game.state.start('menu', true, false, 'options', options_menu); } },
-  { name: 'off', onclick: function () { user_settings.music_enabled = false; game.state.start('menu', true, false, 'options', options_menu); } }
+  { name: 'on',  onclick: function () { user_settings.music_enabled = true;  launch_menu('options', options_menu); } },
+  { name: 'off', onclick: function () { user_settings.music_enabled = false; launch_menu('options', options_menu); } }
 ];
 
 var sounds_menu = [
-  { name: 'on',  onclick: function () { user_settings.sounds_enabled = true;  game.state.start('menu', true, false, 'options', options_menu); } },
-  { name: 'off', onclick: function () { user_settings.sounds_enabled = false; game.state.start('menu', true, false, 'options', options_menu); } }
+  { name: 'on',  onclick: function () { user_settings.sounds_enabled = true;  launch_menu('options', options_menu); } },
+  { name: 'off', onclick: function () { user_settings.sounds_enabled = false; launch_menu('options', options_menu); } }
 ];
 
 window.onload = function() {

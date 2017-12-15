405910e5f530eca65aa72fb3c52fcceac3ca9369
Joe Anderson <joejbajoe@gmail.com>
2017-12-15 23:28:47 +0000

    Skip cutscene
    Seems to be broken on Heroku

--- public/game/tiles/end.js
+++ public/game/tiles/end.js
@@ -7,12 +7,7 @@ class EndTile extends Tile {
     var cutscene = this.data.cutscene;
     var next_lvl = this.data.next_level;
     music.stop();
-    $('#message').text("Well done!");
-    Fade.toBlack(game, 1000, function () {
-      game.state.start("cutscene", true, false, cutscene, function () {
-        game.state.start('playing', true, false, next_lvl);
-      });
-    })
+    game.state.start('playing', true, false, next_lvl);
     return true;
   }
 }

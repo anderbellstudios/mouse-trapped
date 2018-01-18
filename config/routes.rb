1de12c1491a118ffe934303a6e417c1d8a4483ea
Joe Anderson <joejbajoe@gmail.com>
2018-01-18 10:43:47 +0000

    Create subscriptions scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  resources :subscriptions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static#splash'
   get 'play', to: 'static#play'
   get 'about', to: 'static#about'
   get 'instructions', to: 'static#instructions'
   get 'contact', to: 'static#contact'
+  get 'notifyme', to: 'static#notify_me'
+
+  resource :subscriptions
 end

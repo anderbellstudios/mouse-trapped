49d91d0baea7a23686f8cfa2fb209940d48bf8f8
Joe Anderson <joejbajoe@gmail.com>
2018-04-13 23:26:26 +0100

    Redirect to mousetrapped.co.uk

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :redirect_if_old
+
+  protected
+
+  def redirect_if_old
+    if should_redirect?
+      redirect_to "#{request.protocol}www.mousetrapped.co.uk#{request.fullpath}", :status => :moved_permanently
+    end
+  end
+
+  def should_redirect?
+    request.host != 'www.mousetrapped.co.uk' or request.host != 'localhost'
+  end
 end

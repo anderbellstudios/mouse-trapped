8a438bd350b651f4610c386e01e185d160c994a7
Joe Anderson <joejbajoe@gmail.com>
2018-05-27 09:27:38 +0100

    Add generic email template

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -12,8 +12,38 @@ class SubscriptionMailer < ApplicationMailer
     mail(to: @subscription.email, subject: "We've got a new level for you. Come and play!")
   end
 
+  def generic_email(subscription, subject, body)
+    @subscription = subscription
+    @subject = subject
+    @body = body 
+    @html_body = markdown(body)
+    set_unsubscribe_url
+    mail(to: @subscription.email, subject: subject)
+  end
+
   private 
     def set_unsubscribe_url
       @unsubscribe_url = "#{Figaro.env.server_domain}unsubscribe/#{@subscription.id}?security_hash=#{@subscription.security_hash}"
+    end
+
+    def markdown(text)
+      options = {
+        filter_html:     true,
+        hard_wrap:       true,
+        link_attributes: { rel: 'nofollow', target: "_blank" },
+        space_after_headers: true,
+        fenced_code_blocks: true
+      }
+
+      extensions = {
+        autolink:           true,
+        superscript:        true,
+        disable_indented_code_blocks: true
+      }
+
+      renderer = Redcarpet::Render::HTML.new(options)
+      markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+      markdown.render(text).html_safe
     end
 end
--- test/mailers/previews/subscription_mailer_preview.rb
+++ test/mailers/previews/subscription_mailer_preview.rb
@@ -7,4 +7,12 @@ class SubscriptionMailerPreview < ActionMailer::Preview
   def new_level
     SubscriptionMailer.new_level_email(Subscription.first, LevelCode.first)
   end
+
+  def generic_email
+    SubscriptionMailer.generic_email(Subscription.first, "This is the subject field!", %{
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at nisl ipsum. Fusce vehicula sem vitae libero convallis dictum. Pellentesque ultrices est non sodales pretium. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque tortor id suscipit mollis. Praesent venenatis ultrices felis in posuere. Curabitur luctus nibh sed urna tincidunt, pretium egestas neque congue. Nulla posuere turpis a accumsan condimentum. Quisque rutrum dignissim ultricies.
+
+Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In aliquet nulla a gravida finibus. Nulla fringilla lacus eu dui tristique, porta aliquet felis bibendum. Quisque venenatis faucibus nisi, vitae vehicula magna iaculis nec. Ut justo urna, viverra at risus eget, aliquet malesuada est. Duis at lacinia neque. Etiam vitae lectus eget sem iaculis suscipit eget vel tellus. Suspendisse potenti. Sed eleifend orci erat, sed lacinia nulla scelerisque ut. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+})
+  end
 end

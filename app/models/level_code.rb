8aeaa15e02ab0a233efa2939676598a7f5707c4b
Joe Anderson <joejbajoe@gmail.com>
2018-07-09 06:53:51 +0100

    Fix level code sorting on splash page

--- app/models/level_code.rb
+++ app/models/level_code.rb
@@ -1,5 +1,5 @@
 class LevelCode < ApplicationRecord
-  scope :recent, -> { where("created_at > ?", 2.weeks.ago) }
+  scope :recent, -> { where("created_at > ?", 2.weeks.ago).order(:created_at) }
 
   def self.find_level_for code
     LevelCode.find_by(code: code.downcase.strip)

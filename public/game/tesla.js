54f24e49ed5f33af7dc7e293b0a14dc7205378de
Joe Anderson <joejbajoe@gmail.com>
2018-07-13 09:24:08 +0100

    Teslas rotate to follow the mouse

--- public/game/tesla.js
+++ public/game/tesla.js
@@ -6,18 +6,35 @@ class Tesla extends Creature {
   update(time) {
     super.update(time);
 
+    this.sprite.rotation = this.angle;
+    this.sprite.anchor.x = 0.5;
+    this.sprite.anchor.y = 0.5;
+
+    this.sprite.position = absPosfor( posAdd( this.position, { "dx": 0.5, "dy": 0.5 } ) );
+
     if ( this.shouldFire(time) ) {
       this.fire();
     }
   }
 
   fire() {
-    var bolt = new Bolt( this.position, { "dx": -0.1, "dy": 0 } );
+    var bolt = new Bolt( this.position, this.boltVector );
     bolt.create(game);
   }
 
   wasShot(time) {
     return true; // do not die
+  }
+
+  get angle() {
+    return ( Math.PI / 2 ) - Math.atan2( 
+      player.position.x - this.position.x, 
+      player.position.y - this.position.y);  
+  }
+
+  get boltVector() {
+    var speed = 0.2;
+    return { "dx": Math.cos(this.angle) * speed, "dy": Math.sin(this.angle) * speed };
   }
 
   shouldFire(time) {
@@ -26,7 +43,7 @@ class Tesla extends Creature {
       return true;
     }
 
-    if ( this.last_fired + 2000 < time ) {
+    if ( this.last_fired + 100 < time ) {
       this.last_fired = time; 
       return true;
     }

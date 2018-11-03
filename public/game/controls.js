708c0c6167e763da4d97b324c47f04eff607f7be
Joe Anderson <joejbajoe@gmail.com>
2018-11-03 14:14:38 +0000

    Enable directional pad on games controller

--- public/game/controls.js
+++ public/game/controls.js
@@ -4,9 +4,11 @@ var Controls = {
       Control_WASD, 
       Control_HJKL, 
       Control_Arrows, 
-      Control_Buttons 
+      Control_Buttons,
+      Control_DPad
     ].some( function (source) {
-      return source[method]();
+      if ( source.enabled() )
+        return source[method]();
     });
   },
 
@@ -20,35 +22,39 @@ function is_key_down(key_name) {
   return game.input.keyboard.addKey(
     Phaser.Keyboard[key_name]
   ).isDown;
-}
+};
 
 var Control_WASD = {
+  enabled: function() { return true; },
   up:    function() { return is_key_down('W'); },
   down:  function() { return is_key_down('S'); },
   left:  function() { return is_key_down('A'); },
   right: function() { return is_key_down('D'); }
-}
+};
 
 var Control_HJKL = {
+  enabled: function() { return true; },
   up:    function() { return is_key_down('K'); },
   down:  function() { return is_key_down('J'); },
   left:  function() { return is_key_down('H'); },
   right: function() { return is_key_down('L'); }
-}
+};
 
 var Control_Arrows = {
+  enabled: function() { return true; },
   up:    function() { return is_key_down('UP');    },
   down:  function() { return is_key_down('DOWN');  },
   left:  function() { return is_key_down('LEFT');  },
   right: function() { return is_key_down('RIGHT'); }
-}
+};
 
 var Control_Buttons = {
+  enabled: function() { return true; },
   up:    function() { return $('#up-arrow')[0].down;    },
   down:  function() { return $('#down-arrow')[0].down;  },
   left:  function() { return $('#left-arrow')[0].down;  },
   right: function() { return $('#right-arrow')[0].down; }
-}
+};
 
 window.addEventListener("keydown", function(e) {
     // space and arrow keys
@@ -56,3 +62,15 @@ window.addEventListener("keydown", function(e) {
         e.preventDefault();
     }
 }, false);
+
+function gamepad() {
+  return navigator.getGamepads()[0];
+}
+
+var Control_DPad = {
+  enabled: function() { return !!gamepad(); },
+  up:    function() { return gamepad().buttons[12].pressed; },
+  down:  function() { return gamepad().buttons[13].pressed; },
+  right: function() { return gamepad().buttons[15].pressed; },
+  left:  function() { return gamepad().buttons[14].pressed; }
+};

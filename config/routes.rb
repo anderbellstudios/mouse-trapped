d2ca47bb57f0fc113186119fba945cd02d487c40
Joe Anderson <joejbajoe@gmail.com>
2019-04-02 20:58:42 +0100

    Remove references to notify me and news

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,9 @@ Rails.application.routes.draw do
   get 'about', to: 'static#about'
   get 'instructions', to: 'static#instructions'
   get 'contact', to: 'static#contact'
-  get 'notifyme', to: 'subscriptions#new'
   get 'privacy', to: 'static#privacy'
   get 'gameover', to: 'static#gameover'
-  get 'unsubscribe/:id', to: 'subscriptions#destroy'
   get 'level_code/:id', to: 'level_codes#show'
-  resource :subscriptions
-  get 'news', to: 'news#index'
   get 'levelport_locations/create'
   get '/poppixie', to: redirect('https://12joan.github.io/pop-pixie-online/')
 end

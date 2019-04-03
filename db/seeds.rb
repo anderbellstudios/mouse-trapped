1a4af23bbaf5a667d5434c6e3c46c2b9e8484391
Joe Anderson <joejbajoe@gmail.com>
2019-04-03 09:08:43 +0100

    Add level code seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+level_codes = [
+  ['01', 'mouse'],
+  ['02', 'cat'],
+  ['04', 'button'],
+  ['05', 'scientist'],
+  ['06', 'doors'],
+  ['07', 'hidden'],
+  ['08', 'escape'],
+  ['09', 'hide'],
+  ['0a', 'run'],
+  ['0b', 'shadow'],
+  ['0c', 'cycle'],
+  ['0d', 'pathfinder'],
+  ['0e', 'holes'],
+  ['0f', 'friend'],
+  ['10', 'multiple'],
+  ['11', 'traps'],
+  ['12', 'slow'],
+  ['14', 'telsas'],
+  ['15', 'gap'],
+  ['16', 'gate'],
+  ['1e', 'maze'],
+  ['1f', 'buttons'],
+  ['20', 'endings']
+]
+
+level_codes.each { |lc|
+  LevelCode.create level: lc[0], code: lc[1]
+}
